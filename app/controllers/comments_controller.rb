97e4b351caa1797435b1fe1e874c3450c69ca81b
kiitan087 <kiitan087@gmail.com>
2021-06-23 19:01:27 +0900

    編集ボタンのリダイレクト先の編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
-      redirect_to "/prototypes/#{@comment.prototype.id}"
+      redirect_to "/prototypes/#{@comment.prototype_id}"
     else
       @prototype = @comment.prototype
       @comments = @prototype.comments
